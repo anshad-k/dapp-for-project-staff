1f32724371e8ff8cc5df0de3ce209dd5d19827f7
Mohammed Anshad <85294714+anshadkonnola@users.noreply.github.com>
2024-04-26 11:33:45 +0530

    pending: scheduling the transaction

--- src/contracts/ProjectStaffContractIITM.sol
+++ src/contracts/ProjectStaffContractIITM.sol
@@ -15,6 +15,7 @@ contract projectStaffContractIITM {
         address payable facultyAddress;
         string department;
         string email;
+        uint[] projectIds;
     }
 
     struct Project {
@@ -25,14 +26,16 @@ contract projectStaffContractIITM {
         string startDate;
         string endDate;
         uint salary;
+        address payable projectStaffAddress;
         address payable[] faculties;
+        address payable currentApprover;
     }
 
     struct ProjectStaff {
         string name;
         address payable staffAddress;
         string email;
-        Project[] projects;
+        uint[] projectIds;
     }
 
     address payable admin;
@@ -42,6 +45,8 @@ contract projectStaffContractIITM {
 
     ProjectStaff[] private projectStaffs;
     mapping (address => uint) staffMap;
+
+    Project[] private projects;
 
     constructor() public {
         admin = payable(msg.sender);
@@ -60,37 +65,23 @@ contract projectStaffContractIITM {
         name: _name,
         facultyAddress: payable(msg.sender),
         department: _department,
-        email: _email
+        email: _email,
+        projectIds: new uint[](0)
       }));
       facultyMap[msg.sender] = faculties.length; // 1-indexed
       return 2;
     }
 
-    function loginFaculty() external view returns (Project) {
+    function loginFaculty() external view returns (uint) {
       if(facultyMap[msg.sender] == 0) {
         return 4;
       }
-      for(uint i = 0; i < projectStaffs.length; i++) {
-        for(uint j = 0; j < projectStaffs[i].projects.length; j++) {
-          for(uint k = 0; k < projectStaffs[i].projects[j].faculties.length; k++) {
-            if(projectStaffs[i].projects[j].faculties[k] == msg.sender) {
-              return projectStaffs[i].projects[j];
-            }
-          }
-        }
-      }
+      return 2;
     }
 
     function registerProjectStaff(
         string calldata _name, 
-        string calldata _email,
-        string calldata _title,
-        string calldata _description,
-        string calldata _startDate,
-        string calldata _endDate,
-        uint _salary,
-        uint256[] calldata _facultyAddresses
-
+        string calldata _email
       ) external returns (uint) {
       if(facultyMap[msg.sender] != 0) {
         // Already registered
@@ -100,25 +91,109 @@ contract projectStaffContractIITM {
         name: _name,
         staffAddress: payable(msg.sender),
         email: _email,
-        projects: new Project[](0)
+        projectIds: new uint[](0)
       }));
-      projectStaffs[projectStaffs.length - 1].projects.push(Project({
-        id: projectStaffs[projectStaffs.length - 1].projects.length + 1,
+      staffMap[msg.sender] = projectStaffs.length; // 1-indexed
+      return 2;
+    }
+
+    function loginProjectStaff() external view returns (uint) {
+      if(staffMap[msg.sender] == 0) {
+        return 4;
+      }
+      return 2;
+    }
+
+    function addProject(
+      string calldata _title,
+      string calldata _description,
+      string calldata _startDate,
+      string calldata _endDate,
+      uint _salary,
+      uint256[] calldata _facultyAddresses
+    ) external returns (uint) {
+      if(staffMap[msg.sender] == 0 || _facultyAddresses.length == 0) {
+        return 4;
+      }
+      projects.push(Project({
+        id: projects.length + 1, // 1-indexed
         title: _title,
         description: _description,
         status: ProjectStatus.PENDING,
         startDate: _startDate,
         endDate: _endDate,
         salary: _salary,
-        faculties: new address payable[](0)
+        projectStaffAddress: payable(msg.sender),
+        faculties: new address payable[](0),
+        currentApprover: payable(admin)
       }));
+
+      Project storage newProject = projects[projects.length - 1];
+
       for(uint i = 0; i < _facultyAddresses.length; i++) {
-        projectStaffs[projectStaffs.length - 1]
-          .projects[projectStaffs[projectStaffs.length - 1].projects.length - 1]
-          .faculties
-          .push(payable(uint256ToAddress(_facultyAddresses[i])));
+        address payable facultyAddress = payable(uint256ToAddress(_facultyAddresses[i]));
+        newProject.faculties.push(facultyAddress);
+        faculties[facultyMap[facultyAddress] - 1].projectIds.push(projects.length);
       }
-      staffMap[msg.sender] = projectStaffs.length; // 1-indexed
+
+      projectStaffs[staffMap[msg.sender] - 1].projectIds.push(projects.length);
+      newProject.currentApprover = payable(uint256ToAddress(_facultyAddresses[0]));
+      return 2;
+    }
+
+    function scheduleSalaryTransaction(uint _projectId) private returns (uint) {
+      if( _projectId > projects.length) {
+        return 4;
+      }
+      Project storage project = projects[_projectId - 1];
+      if(staffMap[project.projectStaffAddress] == 0) {
+        return 4;
+      }
+      
+      if(project.status != ProjectStatus.APPROVED) {
+        return 5;
+      }
+      if(project.endDate < block.timestamp) {
+        return 6;
+      }
+      project.projectStaffAddress.transfer(project.salary);
+      project.status = ProjectStatus.COMPLETED;
+      return 2;
+    }
+
+    function approveProject(uint calldata _projectId, bool calldata approval) external returns (uint) {
+      if(facultyMap[msg.sender] == 0 || _projectId > projects.length) {
+        return 4;
+      }
+      Project storage project = projects[_projectId - 1];
+      if(project.status != ProjectStatus.PENDING && project.status != ProjectStatus.EXTENSION) {
+        return 5;
+      }
+      if(project.currentApprover != msg.sender) {
+        return 6;
+      }
+      if(approval) {
+        project.status = ProjectStatus.APPROVED;
+        scheculeSalaryTransaction(_projectId);
+      } else {
+        project.status = ProjectStatus.REJECTED;
+      }
+      return 2;
+    }
+
+    function getExtension(
+        uint calldata _projectId, 
+        string calldata _newEndDate,
+      ) external returns (uint) {
+      if(projectStaffs[msg.sender] == 0 || _projectId > projects.length) {
+        return 4;
+      }
+      Project storage project = projects[_projectId - 1];
+      if(project.status != ProjectStatus.APPROVED) {
+        return 5;
+      }
+      project.status = ProjectStatus.EXTENSION;
+      project.endDate = _newEndDate;
       return 2;
     }
 
@@ -126,12 +201,16 @@ contract projectStaffContractIITM {
       return faculties;
     }
 
-    function addressToUint256(address _addr) private returns (uint256) {
+    function getAllProjects() external view returns (Project[] memory) {
+      return projects;
+    }
+
+    function addressToUint256(address _addr) private pure returns (uint256) {
         uint160 _uint = uint160(_addr);
         return uint256(_uint);
     }
 
-    function uint256ToAddress(uint256 _uint) private returns (address) {
+    function uint256ToAddress(uint256 _uint) private pure returns (address) {
         return address(uint160(_uint));
     }
 
